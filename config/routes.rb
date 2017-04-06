52fd9296d12cf03224b366441aec0b80eb3c0f0b
takahashi yuki <gyjxserh@gmail.com>
2017-04-06 23:10:40 +0900

    rails tutorial7

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get  '/about',to:'staticpages#about'
   get  '/contact',to:'staticpages#contact'
   get '/signup',to:'users#new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
